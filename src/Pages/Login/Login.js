1ecc6454b6ffdbd276fcb55bfb3f836a22d6cda2
Joao Augusto <joaoaugustofrezende@outlook.com>
2023-01-06 16:24:56 -0300

    background style added

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from './Styles'
+
+const Login = () => {
+  return (
+    <Container>
+        
+    </Container>
+  )
+}
+
+export default Login
---
+++ src/Pages/Login/Styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-image: linear-gradient(to bottom, #30103B, #411C54);
+    width: 100vw;
+    height: 100vh;
+`;
